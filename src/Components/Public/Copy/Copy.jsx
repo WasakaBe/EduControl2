11c3398b1b7b74b3ea8ccb9e857d67ec4dc99086
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2023-11-14 21:07:10 -0600

    Delete src/Components/Public directory

--- src/Components/Public/Copy/Copy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Copy.css'
-export default function Copy() {
-  return (
-    <div className='copy'>
-      <p>Todos los derechos reservados | CBTA5 © 2023</p>
-    </div>
-  )
-}
